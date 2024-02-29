af0895630f012f0e799a67e4e75cd7ddd519b094
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-02-29 09:45:26 +0800

    Remove search functionality

--- assets/js/search.js
+++
@@ -1,9 +0,0 @@
-import params from "@params";
-
-const inputElement = document.getElementById("search-input");
-const linkElement = document.getElementById("generated-link");
-const siteQuery = `https://duckduckgo.com/?q=${encodeURIComponent("site:" + params.baseUrl)}`;
-
-inputElement.addEventListener("keyup", () => {
-  linkElement.href = `${siteQuery}+${encodeURIComponent(inputElement.value)}`;
-});
